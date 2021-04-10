9985382938762ebcee44a3382b5234ed971a6bb0
mukhammadarifin <54782878+mukhammadarifin@users.noreply.github.com>
2021-04-10 12:22:49 +0700

    Upload Project UTS Pertemuan 9

---
+++ UTS/UTS/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //VARIABLES
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+
+    private Vector3 moveDirection;
+    private Vector3 velocity;
+
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float gravity;
+
+    [SerializeField] private float jumpHeight;
+
+    // REFREMCES
+    private CharacterController controller;
+    private Animator anim;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        Move();
+
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            StartCoroutine(Attack());
+        }
+    }
+
+    private void Move()
+    {
+        isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask); 
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float moveZ = Input.GetAxis("Vertical");
+
+        moveDirection = new Vector3(0, 0, moveZ);
+        moveDirection = transform.TransformDirection(moveDirection);
+
+
+        if(isGrounded)
+        {
+            if(moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
+            {
+                // walk
+                Walk();
+
+            }
+            else if(moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
+            {
+                //Run
+                Run();
+            }
+            else if(moveDirection == Vector3.zero)
+            {
+                //Idle
+                Idle();
+            }
+
+            moveDirection *= moveSpeed;
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
+            }
+        }
+
+        controller.Move(moveDirection * Time.deltaTime);
+    
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void Idle()
+    {
+        anim.SetFloat("Speed", 0, 0.1f, Time.deltaTime);
+    }
+
+    private void Walk()
+    {
+        moveSpeed = walkSpeed;
+        anim.SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
+    }
+
+    private void Run()
+    {
+        moveSpeed = runSpeed;
+        anim.SetFloat("Speed", 1, 0.1f, Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+    }
+
+    private IEnumerator Attack()
+    {
+        anim.SetLayerWeight(anim.GetLayerIndex("Attack Layer"), 1);
+        anim.SetTrigger("Attack");
+
+        yield return new WaitForSeconds(0.9f);
+        anim.SetLayerWeight(anim.GetLayerIndex("Attack Layer"), 0);
+    }
+
+}
---
+++ UTS/UTS/Assets/Scripts/UpdateProbe.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpdateProbe : MonoBehaviour
+{
+    private ReflectionProbe probe;
+    
+    void Awake()
+    {
+        probe = GetComponent<ReflectionProbe>();
+        probe.RenderProbe();
+    }
+    
+    public void RefreshProbe()
+    {
+        probe.RenderProbe();
+    }
+}
